6bfa4e6c88860cc92eb125df2e8de07cb5f60b4c
kendynweb <kendynweb@gmail.com>
2023-10-11 09:20:17 -0400

    Basic server running

---
+++ server.js
@@ -0,0 +1,69 @@
+
+/////////////////////////////////
+// DEPENDENCIES 
+////////////////////////////////
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+const app = express();
+
+////////////////////////////////
+// DATABASE CONNECTION 
+////////////////////////////////
+// Database not connected, yet
+// const { DATABASE_URL } = process.env;
+
+// Mongoose connection
+
+// mongoose.connect(DATABASE_URL)
+
+// Mongoose connection messages
+mongoose.connection
+    .on('open', () => console.log('You are connected to mongoose'))
+    .on('close', () => console.log('You are disconnected to mongoose'))
+    .on('error', (err) => console.log(err))
+
+
+////////////////////////////////
+// MODELS 
+////////////////////////////////
+
+
+////////////////////////////////
+// MIDDLEWARE
+////////////////////////////////
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+////////////////////////////////
+// ROUTES 
+////////////////////////////////
+
+// TEST Route
+app.get('/', (req, res) => {
+  res.send('THIS IS A TEST ROUTE!');
+});
+
+
+// INDEX ROUTE
+
+
+
+
+// CREATE ROUTE 
+
+
+// UPDATE ROUTE
+
+// DESTROY/DELETE ROUTE 
+
+
+////////////////////////////////
+// LISTENER 
+////////////////////////////////
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
