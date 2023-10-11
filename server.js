9f039a7644ba13f79ac4a3fe9edf64b62543b516
kendynweb <kendynweb@gmail.com>
2023-10-11 19:01:02 -0400

    updated server and port

--- server.js
+++ server.js
@@ -65,5 +65,5 @@ app.get('/', (req, res) => {
 ////////////////////////////////
 // LISTENER 
 ////////////////////////////////
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
