15c9b9f05f1210baee397ceddaeb76bd5daeb413
kendynweb <kendynweb@gmail.com>
2023-10-12 19:30:36 -0400

    Basic Error handling for routes

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -10,22 +10,38 @@ const router = express.Router()
 
 // POST - CREATE a new photo
 router.post('/upload', (req, res) => {
-    // upload logic
+    try {
+      // upload logic
+    } catch (error) {
+        res.status (400).send(error)
+    }
 })
 
 // GET - READ a single photo
 router.get("/:id", async (req, res) => {
-
+    try {
+        // read logic
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 // DELETE - DELETE a single photo
 router.delete("/:id", async (req, res) => {
-
+     try {
+       // delete logic
+     } catch (error) {
+       res.status(500).send(error);
+     }
 })
 
 // GET - READ all photos
 router.get('/', async (req, res) => {
-    
+      try {
+        // read all logic
+      } catch (error) {
+        res.status(500).send(error);
+      }
 })
 
 // Export router
