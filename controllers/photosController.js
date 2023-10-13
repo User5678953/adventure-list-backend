90bead79445485a697706dd8c1fd8a24652e6a5a
kendynweb <kendynweb@gmail.com>
2023-10-12 20:10:20 -0400

    res.send routes

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -11,6 +11,7 @@ const router = express.Router()
 // POST - CREATE a new photo
 router.post('/upload', (req, res) => {
     try {
+        res.send("Photo upload route is working!")
       // upload logic
     } catch (error) {
         res.status (400).send(error)
@@ -20,6 +21,7 @@ router.post('/upload', (req, res) => {
 // GET - READ a single photo
 router.get("/:id", async (req, res) => {
     try {
+        res.send("Read single photo route is working!")
         // read logic
     } catch (error) {
         res.status(500).send(error)
@@ -28,19 +30,21 @@ router.get("/:id", async (req, res) => {
 
 // DELETE - DELETE a single photo
 router.delete("/:id", async (req, res) => {
-     try {
-       // delete logic
+    try {
+        res.send("Delete single photo route is working!")
+        // delete logic
      } catch (error) {
-       res.status(500).send(error);
+       res.status(500).send(error)
      }
 })
 
 // GET - READ all photos
 router.get('/', async (req, res) => {
-      try {
+    try {
+        res.send("Read all photos route is working!")
         // read all logic
       } catch (error) {
-        res.status(500).send(error);
+        res.status(500).send(error)
       }
 })
 
